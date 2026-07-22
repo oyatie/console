c2ea72fa205dc2cca6502ebdc4104c16d49d530c
Jason Lee <56489493+jason931225@users.noreply.github.com>
2026-07-22 00:18:40 -0400

    Delete coss-rn directory

--- coss-rn/jest.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  testMatch: ['<rootDir>/src/**/*.test.ts?(x)'],
-};
--- coss-rn/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import CossLandingScreen from './CossLandingScreen';
-
-export default function App() {
-  return <CossLandingScreen />;
-}
